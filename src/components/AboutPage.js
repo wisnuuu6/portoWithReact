9758906ebd5298eb16c0a7b6954509517ba416c5
wisnuuu6 <wisnuyoga32@gmail.com>
2023-07-11 23:13:41 +0700

    pertama commit

---
+++ src/components/AboutPage.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import styled, { keyframes, ThemeProvider } from 'styled-components'
+import {DarkTheme} from './Themes';
+
+
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcons from '../subComponents/SocialIcons';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent';
+import BigTitle from '../subComponents/BigTitlte'
+import astronaut from '../assets/Images/spaceman.png'
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height:100vh;
+position: relative;
+overflow: hidden;
+`
+const float = keyframes`
+0% { transform: translateY(-10px) }
+50% { transform: translateY(15px) translateX(15px) }
+100% { transform: translateY(-10px) }
+
+`
+const Spaceman = styled.div`
+position: absolute;
+top: 10%;
+right: 5%;
+width: 20vw;
+animation: ${float} 4s ease infinite;
+img{
+    width: 100%;
+    height: auto;
+}
+`
+const Main =  styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 50vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(0.6rem + 1vw);
+ backdrop-filter: blur(4px);
+  
+  position: absolute;
+  left: calc(5rem + 5vw);
+  top: 10rem;
+  font-family: 'Ubuntu Mono', monospace;
+  font-style: italic;
+`
+
+
+
+
+const AboutPage = () => {
+    return (
+        <ThemeProvider theme={DarkTheme}>
+<Box>
+
+<LogoComponent theme='dark'/>
+<SocialIcons theme='dark'/>
+<PowerButton />
+<ParticleComponent theme='dark' />
+
+        <Spaceman>
+            <img src={astronaut} alt="spaceman" />
+        </Spaceman>    
+        <Main>
+        introduce my name is wisnu yoga permana, I was born in Tegal, I am currently 19 years old, I am a student and also an IT System Administrator in an IT company who have worked for approximately 2 years. For more details, see my CV below.
+        </Main>
+
+        <BigTitle text="ABOUT" top="10%" left="5%" />
+
+
+        </Box>
+
+        </ThemeProvider>
+        
+    )
+}
+
+export default AboutPage
